2edecb9843567f99a262379b44b0b77736db518d
maarief64 <maarief64@gmail.com>
2019-06-13 19:34:55 +0700

    final code

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,11 @@
+<footer id="footer">
+
+            <!-- FOOTER BOTTOM -->
+            <div class="footer_bottom">
+                <div class="container">
+                    <p>&copy; <?php echo date('Y');?> M Hotel All rights reserved | Copyright By <style="color: #fff;">Achmad Ma'arief</a> </p>
+                </div>
+            </div>
+            <!-- END / FOOTER BOTTOM -->
+
+        </footer>
